874589c9491153204b225a667ad378c92d165b41
sureshthumma <suresh@stackaero.io>
2019-02-27 00:53:02 -0500

    updated

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,5 +1,5 @@
-export const LOCAL_BASE_URL = 'http://localhost:2021/api/';
-export const IMAGE_URL = 'http://localhost:2021/profile_image/';
+export const LOCAL_BASE_URL = process.env.API_URL+'/api/';
+export const IMAGE_URL = process.env.API_URL+'/profile_image/';
 //export const LOCAL_BASE_URL = 'https://gymapplication-218011.appspot.com/api/';
 //export const IMAGE_URL = 'https://gymapplication-218011.appspot.com/profile_image/';
 export const ITEM_COUNT_PER_PAGE = 10;
